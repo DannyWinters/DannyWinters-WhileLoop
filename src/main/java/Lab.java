aeea05e09bb220243232be893f2fcdd0fedf25b6
Danny <serena.nwinters@gmail.com>
2025-05-15 20:17:54 +0000

    Complete While Loop Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -36,7 +36,15 @@ public class Lab {
         int sum = 0;
         int i = 0;
 
-//        code here
+
+        //        code here
+        while (i < end){
+        sum = sum + i;
+        i++;
+        
+        System.out.println("The sum is currently: "+sum);
+        System.out.println("The int i is currently: "+i);
+        }
 
         return sum;
     }
